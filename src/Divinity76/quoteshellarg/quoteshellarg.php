2ae00ac1277f0e40d32dcd353184090e7f6c402b
divinity76 <hans@loltek.net>
2024-09-19 07:52:45 +0200

    support int|float

--- src/Divinity76/quoteshellarg/quoteshellarg.php
+++ src/Divinity76/quoteshellarg/quoteshellarg.php
@@ -1,33 +1,45 @@
 <?php
-declare (strict_types = 1);
+
+declare(strict_types=1);
+
 namespace Divinity76\quoteshellarg;
 
 /**
  * quotes shell arguments
  * (doing a better job than escapeshellarg)
  *
- * @param string $arg
- * @throws UnexpectedValueException if $arg contains null bytes
+ * @param string|int|float $arg
+ * @throws \UnexpectedValueException if $arg contains null bytes
  * @return string
  */
 
-function quoteshellarg(string $arg): string
+function quoteshellarg($arg): string
 {
+    if (\is_float($arg)) {
+        // 17: >The 53-bit significand precision gives from 15 to 17 significant decimal digits precision.
+        return \escapeshellarg(\sprintf('%.17g', $arg));
+    }
+    if (\is_int($arg)) {
+        return \escapeshellarg((string) $arg);
+    }
+    if (!\is_string($arg)) {
+        throw new \InvalidArgumentException('Argument #1 ($arg) must be of type string|int|float, ' . (is_object($arg) ? get_class($arg) : gettype($arg)) . ' given');
+    }
     static $isUnix = null;
     if ($isUnix === null) {
-        $isUnix = in_array(PHP_OS_FAMILY, array('Linux', 'BSD', 'Darwin', 'Solaris'), true) || PHP_OS === 'CYGWIN';
+        $isUnix = \in_array(PHP_OS_FAMILY, array('Linux', 'BSD', 'Darwin', 'Solaris'), true) || PHP_OS === 'CYGWIN';
     }
     if ($isUnix) {
         // PHP's built-in escapeshellarg() for unix is kindof garbage: https://3v4l.org/Hkv7h
         // corrupting-or-stripping UTF-8 unicode characters like "æøå" and non-printable characters like "\x01",
         // both of which are fully legal in unix shell arguments.
         // In single-quoted-unix-shell-arguments there are only 2 bytes that needs special attention: \x00 and \x27
-        if (false !== strpos($arg, "\x00")) {
-            throw new UnexpectedValueException('unix shell arguments cannot contain null bytes!');
+        if (false !== \strpos($arg, "\x00")) {
+            throw new \UnexpectedValueException('unix shell arguments cannot contain null bytes!');
         }
-        return "'" . strtr($arg, array("'" => "'\\''")) . "'";
+        return "'" . \strtr($arg, array("'" => "'\\''")) . "'";
     }
     // todo: quoteshellarg for windows? it's a nightmare though: https://docs.microsoft.com/en-us/archive/blogs/twistylittlepassagesallalike/everyone-quotes-command-line-arguments-the-wrong-way
     // fallback to php's builtin escapeshellarg
-    return escapeshellarg($arg);
+    return \escapeshellarg($arg);
 }
